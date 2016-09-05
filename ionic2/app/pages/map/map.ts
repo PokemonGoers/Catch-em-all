e494ce4ea3992ecade87a0177c1f8ee7e3dab4d9
Jochen Hartl <jochen.hartl@tum.de>
2016-09-05 15:40:40 +0200

    Skeleton of search bar

--- ionic2/app/pages/map/map.ts
+++ ionic2/app/pages/map/map.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Searchbar} from "../../components/searchbar/searchbar";
 
 @Component({
-  templateUrl: 'pages/map/map.html'
+  templateUrl: 'pages/map/map.html',
+  directives: [Searchbar]
 })
 export class MapPage {
 
