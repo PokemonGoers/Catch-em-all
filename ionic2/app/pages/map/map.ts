c23b1c3b436799fc9c1c7b6437bc03aa48d61fc5
Wolfgang Hobmaier <wolfgang.hobmaier@datapweb.de>
2016-09-10 17:33:45 +0200

    Cleanup imports

--- ionic2/app/pages/map/map.ts
+++ ionic2/app/pages/map/map.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'pages/map/map.html'
 })
 export class MapPage {
 
-  constructor(private navCtrl: NavController) {
+  constructor() {
 
   }
 
