bbc96817dec7431fa87293421acc4eca43341bfa
Jochen Hartl <jochen.hartl@tum.de>
2016-09-10 22:13:55 +0200

    Make searchbar slide in and out

--- ionic2/app/components/navbar/navbar.ts
+++ ionic2/app/components/navbar/navbar.ts
@@ -1,13 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, animate, trigger, state, style, transition } from '@angular/core';
 import { Searchbar } from '../searchbar/searchbar';
 
 @Component({
   templateUrl: 'components/navbar/navbar.html',
   selector: 'navbar',
-  directives: [Searchbar]
+  directives: [Searchbar],
+  animations: [
+    trigger('searchbarAnimate', [
+      state('visible', style({transform: 'translateX(0)', opacity: 1})),
+      state('hidden', style({transform: 'translateX(100%)', opacity: 0})),
+      transition('visible <=> hidden', animate('300ms ease-in')),
+    ])
+  ]
 })
 export class Navbar {
 
   @Input() title: string;
+
+  searchbarState: string = 'hidden';
+
+  toggleSearchBar() {
+    this.searchbarState = this.searchbarState == 'visible' ? 'hidden' : 'visible';
+  }
 
 }
