6aeb1efafcfa83a2bd803d225c1ce04556a48fdb
Alexander Lill <alexander.lill@tum.de>
2016-09-13 20:08:56 +0200

    Add poke-details component files

---
+++ ionic2/app/components/poke-details/poke-attack.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'components/poke-details/poke-attack.component.html',
+  selector: 'poke-attack'
+})
+
+export class PokeAttackComponent {
+
+  constructor() {}
+
+}
---
+++ ionic2/app/components/poke-details/poke-type.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'components/poke-details/poke-type.component.html',
+  selector: 'poke-type'
+})
+
+export class PokeTypeComponent {
+
+  constructor() {}
+
+}
