82303c4804f9189455864e9801d616c104a3d956
Alexander Lill <alexander.lill@tum.de>
2016-09-13 20:08:56 +0200

    Add poke-details component files

---
+++ ionic2/app/components/poke-details/poke-evolution.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'components/poke-details/poke-evolution.component.html',
+  selector: 'poke-evolution'
+})
+
+export class PokeEvolutionComponent {
+
+  constructor() {}
+
+}
---
+++ ionic2/app/components/poke-details/poke-stats.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'components/poke-details/poke-stats.component.html',
+  selector: 'poke-stats'
+})
+
+export class PokeStatsComponent {
+
+  constructor() {}
+
+}
