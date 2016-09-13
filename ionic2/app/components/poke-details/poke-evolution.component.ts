327eff6002b68608a84e561a48320ef4d3bebc36
Alexander Lill <alexander.lill@tum.de>
2016-09-14 01:28:13 +0200

    Finish poke-evolution component

--- ionic2/app/components/poke-details/poke-evolution.component.ts
+++ ionic2/app/components/poke-details/poke-evolution.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Pokemon } from "../../models/pokemon";
+import { ApiService } from '../../services/api.service';
 
 @Component({
   templateUrl: 'components/poke-details/poke-evolution.component.html',
@@ -10,11 +11,26 @@ export class PokeEvolutionComponent {
 
   @Input("pokemon") pokemon: Pokemon;
 
-  constructor() {
+  public prevPokemon: Pokemon;
+  public nextPokemons: Pokemon[];
+
+  constructor(private apiservice: ApiService) {
+    this.nextPokemons = [];
   }
 
   ngOnInit() {
-    console.log(this.pokemon.fleeRate);
-  }
+    if (this.pokemon.previousEvolutions.length > 0) {
+      this.apiservice.getPokemonById(this.pokemon.previousEvolutions[0].pokemonId)
+        .subscribe(results => this.prevPokemon = results, error => this.prevPokemon = null);
+    }
 
+    for (var i=0, len=this.pokemon.nextEvolutions.length; i<len; i++) {
+      this.apiservice.getPokemonById(this.pokemon.nextEvolutions[i].pokemonId)
+        .subscribe(results => this.nextPokemons.push(results));
+    }
+
+    //FIXME: Remove this; for debugging of multiple evolutions only
+    /*this.apiservice.getPokemonById(4)
+      .subscribe(results => this.nextPokemons.push(results));*/
+  }
 }
