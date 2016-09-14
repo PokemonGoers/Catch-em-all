5415f0e5a6b11259839f9c3f95c15731091b09f2
Wolfgang Hobmaier <wolfgang.hobmaier@datapweb.de>
2016-09-14 19:48:06 +0200

    Simplify switching Tabs

--- ionic2/app/components/filter-popover/filter-popover.component.ts
+++ ionic2/app/components/filter-popover/filter-popover.component.ts
@@ -8,9 +8,9 @@ import {Events, ViewController} from "ionic-angular";
     <ion-content>
         <div [ngSwitch]="currentTab">
             <ion-toolbar style="padding: 0;">
-                <ion-segment>
-                    <ion-segment-button value="time" (click)="currentTab = 'time'">Time</ion-segment-button>
-                    <ion-segment-button value="pokemon" (click)="currentTab = 'pokemon'">Pokemon</ion-segment-button>
+                <ion-segment [(ngModel)]="currentTab" primary>
+                    <ion-segment-button value="time">Time</ion-segment-button>
+                    <ion-segment-button value="pokemon">Pokemon</ion-segment-button>
                 </ion-segment>
             </ion-toolbar>
           <time-tab *ngSwitchCase="'time'"></time-tab>
