0880dd2e5a02c3201be5d89d7a8f6ac2b2a48df8
Jochen Hartl <jochen.hartl@tum.de>
2016-09-19 15:31:06 +0200

    Transpile ES6 sources

--- ionic2/webpack.config.js
+++ ionic2/webpack.config.js
@@ -53,6 +53,8 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.ts$/, loader: 'awesome-typescript-loader'},
+      {test: /\/pokemap-(1|2)\/.*\.js$/, loader: 'babel?presets[]=es2015'}, // Hack to transpile ES6 until modules are packaged
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel?presets[]=es2015'},
       {test: /\.html$/, loader: 'raw'},
       {test: /\/theme\/.*\.scss$/, loader:ExtractTextPlugin.extract(['css', 'sass'])},
       {test: /\.(component|page)\.scss$/, loaders: ['raw', 'sass']},
