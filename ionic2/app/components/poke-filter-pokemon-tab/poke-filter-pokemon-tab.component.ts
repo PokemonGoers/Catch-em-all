949c3d559e485162ff2f6c1ed883a0c7bb596ef7
Alexander Lill <alexander.lill@tum.de>
2016-09-22 16:56:45 +0200

    Unfinished draft poke-filter-pokemon-tab

--- ionic2/app/components/poke-filter-pokemon-tab/poke-filter-pokemon-tab.component.ts
+++ ionic2/app/components/poke-filter-pokemon-tab/poke-filter-pokemon-tab.component.ts
@@ -1,9 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Subscription } from 'rxjs';
+import { Pokemon } from '../../models/pokemon';
+
+type PokemonContainer = {pokemon:Pokemon, isSelected:boolean};
 
 @Component({
-  selector: 'poke-filter-pokemon-tab',
-  template: `<ion-content>Pokemon</ion-content>`
+  template: require('./poke-filter-pokemon-tab.component.html'),
+  selector: 'poke-filter-pokemon-tab'
 })
-export class PokeFilterPokemonTabComponent {
-  constructor() {}
+
+export class PokeFilterPokemonTabComponent implements OnInit, OnDestroy {
+
+  querySubscription: Subscription;
+  pokemonContainers: PokemonContainer[] = [];
+
+  constructor(private apiservice: ApiService) {
+  }
+
+  ngOnInit() {
+    this.querySubscription = this.apiservice.getAllPokemon()
+      .map(pokemonList => {
+        console.log('list', pokemonList);
+        return pokemonList.map(pokemon => {
+          return {
+            pokemon: pokemon,
+            isSelected: true
+          };
+        })
+      }).subscribe(
+        pokemonContainers => this.pokemonContainers = pokemonContainers,
+        error => this.pokemonContainers = []
+      );
+  }
+
+  ngOnDestroy() {
+    this.cancelRequests();
+  }
+
+  getFilteredPokemon() {
+    return this.pokemonContainers.filter(pokemonContainer => this.filter(pokemonContainer.pokemon));
+  }
+
+  filter(pokemon: Pokemon) {
+    return pokemon.types.indexOf('water') >= 0;
+  }
+
+  cancelRequests() {
+    if (this.querySubscription && !this.querySubscription.isUnsubscribed) {
+      this.querySubscription.unsubscribe();
+    }
+  }
+
+  filterChanged() {
+    let selectedPokemon: number[] = [];
+
+    for (let pokemonCon of this.pokemonContainers) {
+      if (pokemonCon.isSelected) {
+          selectedPokemon.push(pokemonCon.pokemon.pokemonId);
+      }
+    }
+
+    console.log(selectedPokemon);
+  }
 }
