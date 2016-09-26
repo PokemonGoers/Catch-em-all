399bb3330e871dcbcf5932aef63e78ae9106cb22
Alexander Lill <alexander.lill@tum.de>
2016-09-26 02:17:39 +0200

    (WIP) Implement pokemon filter view

--- ionic2/app/components/poke-filter-pokemon-tab/poke-filter-pokemon-tab.component.ts
+++ ionic2/app/components/poke-filter-pokemon-tab/poke-filter-pokemon-tab.component.ts
@@ -1,19 +1,61 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Subscription } from 'rxjs';
 import { Pokemon } from '../../models/pokemon';
+import { Pipe, PipeTransform } from '@angular/core';
+import { PokemonFilterPipe } from './pokemon-filter-pipe';
+import { PokemonFilterData } from './pokemon-filter-data';
 
-type PokemonContainer = {pokemon:Pokemon, isSelected:boolean};
+type PokemonContainer = {pokemon: Pokemon, isSelected: boolean};
+type TypeContainer = {
+  type: string,
+  isSelected: boolean
+};
+
+const typeNames: string[] = [
+  'fire',
+  'ice',
+  'poison',
+  'flying',
+  'bug',
+  'grass',
+  'water',
+  'ground',
+  'rock',
+  'fight',
+  'steel',
+  'dragon',
+  'fairy',
+  'dark',
+  'ghost',
+  'psychic',
+  'electric',
+  'normal'
+];
 
 @Component({
   template: require('./poke-filter-pokemon-tab.component.html'),
-  selector: 'poke-filter-pokemon-tab'
+  selector: 'poke-filter-pokemon-tab',
+  pipes: [ PokemonFilterPipe ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class PokeFilterPokemonTabComponent implements OnInit, OnDestroy {
+export class PokeFilterPokemonTabComponent implements OnInit {
+
+  nameFilter: string;
+  typesFilter: string[];
+
+  typeDataBinding: TypeContainer[] = [];
 
   querySubscription: Subscription;
   pokemonContainers: PokemonContainer[] = [];
+
+  pokeFilterData: PokemonFilterData = {
+    pokemonName: "",
+    pokemonTypes: []
+  }
+
+  testo: string[] = [ "abc", "def", "ghi", "huhu", "as", "rtzuio" ];
 
   constructor(private apiservice: ApiService) {
   }
@@ -21,7 +63,6 @@ export class PokeFilterPokemonTabComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.querySubscription = this.apiservice.getAllPokemon()
       .map(pokemonList => {
-        console.log('list', pokemonList);
         return pokemonList.map(pokemon => {
           return {
             pokemon: pokemon,
@@ -32,18 +73,18 @@ export class PokeFilterPokemonTabComponent implements OnInit, OnDestroy {
         pokemonContainers => this.pokemonContainers = pokemonContainers,
         error => this.pokemonContainers = []
       );
+      console.log("CONT: " + this.pokemonContainers);
+
+    for (let str of typeNames) {
+      this.typeDataBinding.push({
+        type: str,
+        isSelected: false
+      });
+    }
   }
 
-  ngOnDestroy() {
+  ionViewDidUnload() {
     this.cancelRequests();
-  }
-
-  getFilteredPokemon() {
-    return this.pokemonContainers.filter(pokemonContainer => this.filter(pokemonContainer.pokemon));
-  }
-
-  filter(pokemon: Pokemon) {
-    return pokemon.types.indexOf('water') >= 0;
   }
 
   cancelRequests() {
@@ -52,7 +93,37 @@ export class PokeFilterPokemonTabComponent implements OnInit, OnDestroy {
     }
   }
 
-  filterChanged() {
+  onNameInput() {
+    this.pokeFilterData.pokemonName = this.nameFilter;
+    console.log("FILTER: " + this.pokeFilterData);
+  }
+
+  onSearch() {
+    // Triggered when the confirm button (e.g. enter) is pressed.
+  }
+
+  typeFilterChanged() {
+    this.pokeFilterData.pokemonTypes = [];
+    for (let typeField of this.typeDataBinding) {
+      if (typeField.isSelected) {
+        this.pokeFilterData.pokemonTypes.push(typeField.type);
+      }
+    }
+  }
+
+  selectAll() {
+    for (let cont of this.pokemonContainers) {
+      cont.isSelected = true;
+    }
+  }
+
+  selectNone() {
+    for (let cont of this.pokemonContainers) {
+      cont.isSelected = false;
+    }
+  }
+
+  applyFilters() {
     let selectedPokemon: number[] = [];
 
     for (let pokemonCon of this.pokemonContainers) {
@@ -61,6 +132,6 @@ export class PokeFilterPokemonTabComponent implements OnInit, OnDestroy {
       }
     }
 
-    console.log(selectedPokemon);
+    console.log("SELECTED:" + selectedPokemon);
   }
 }
---
+++ ionic2/app/components/poke-filter-pokemon-tab/pokemon-filter-data.ts
@@ -0,0 +1,4 @@
+export type PokemonFilterData = {
+  pokemonName: string,
+  pokemonTypes: string[]
+};
