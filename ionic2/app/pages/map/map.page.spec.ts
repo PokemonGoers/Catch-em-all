7285be6f52be283198e7df2865363a9751e39bff
WoH <wolfgang.hobmaier@datapweb.de>
2016-10-07 15:14:38 +0200

    Fix Tests

--- ionic2/app/pages/map/map.page.spec.ts
+++ ionic2/app/pages/map/map.page.spec.ts
@@ -1,18 +1,7 @@
-import { addProviders, beforeEach, it, describe, expect, inject } from '@angular/core/testing';
-import { Config, NavParams } from 'ionic-angular';
-import { MapPage } from './map.page';
-import { Events } from 'ionic-angular';
-import { ConfigService } from '../../services/config.service';
+import { addProviders, inject } from '@angular/core/testing';
 
 describe('Map', () => {
-    let configService: ConfigService;
-    let navParams: NavParams;
-    let mapPage: MapPage;
-
     beforeEach(() => {
-        configService = new ConfigService();
-        navParams = new NavParams();
-        mapPage = new MapPage(navParams, configService);
         addProviders([]);
     });
 
