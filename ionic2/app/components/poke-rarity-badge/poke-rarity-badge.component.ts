cb183d1a8c5cc267f8c2e0d69df596158b0960c5
WoH <wolfgang.hobmaier@datapweb.de>
2016-10-08 19:28:43 +0200

    Code Style and a bit of CSS

--- ionic2/app/components/poke-rarity-badge/poke-rarity-badge.component.ts
+++ ionic2/app/components/poke-rarity-badge/poke-rarity-badge.component.ts
@@ -9,21 +9,8 @@ import { Pokemon } from '../../models/pokemon';
 
 export class PokeRarityBadgeComponent {
 
-  @Input() pokemon: Pokemon;
+  @Input() rarity: number;
 
-  getPokemonRarityStars() {
-    let inHundred = this.pokemon.appearanceLikelihood * 100;
-
-    if (inHundred < 1) {
-      let inThousand = inHundred * 10;
-      if (inThousand < 1) {
-        return [0, 0, 0];
-      } else {
-        return [0, 0];
-      }
-    } else {
-      return [0];
-    }    
-  }
+  constructor() { }
 
 }
