57caac8c703dcfe137264a248d18c24d1929fbc2
Phil Buschmann <phil.buschmann@tum.de>
2016-10-10 22:10:56 +0200

    Redesign and fix of imprint page for ios. Changed wording in about page

--- ionic2/app/pages/imprint/imprint.page.ts
+++ ionic2/app/pages/imprint/imprint.page.ts
@@ -1,8 +1,12 @@
-import { Page } from 'ionic-angular';
+import { forwardRef } from '@angular/core';
+import { Page, NavController } from 'ionic-angular';
 
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Page({
-  template: require('./imprint.page.html')
+  template: require('./imprint.page.html'),
+  styles: [require('../about/about.page.scss')],
+  directives: [forwardRef(() => NavbarComponent)]
 })
 
 export class ImprintPage {
