984973d8265ada847ae90ac094ead0921c5df7ee
WoH <wolfgang.hobmaier@datapweb.de>
2016-10-18 16:22:17 +0200

    package.json Bump to RC1

---
+++ ionic2/rollup.config.js
@@ -0,0 +1,22 @@
+const config = require('@ionic/app-scripts/config/rollup.config');
+const replace = require('rollup-plugin-replace');
+
+var release = process.env['IONIC_ENV'] === 'prod';
+
+const env = {
+  BUILD_ENV: release ? 'release' : 'develop',
+  BUILD_TIME: new Date(),
+  API_ENDPOINT: process.env['API_ENDPOINT'] || 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014'
+};
+
+for (let envKey in env) {
+  console.log(envKey, env[envKey]);
+}
+
+config.plugins.push(replace({
+  include: '**/.tmp/app/env.js',
+  delimiters: ['<@', '@>'],
+  values: env
+}));
+
+module.exports = config;
