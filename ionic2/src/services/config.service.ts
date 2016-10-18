92c54623d1544e87bf26393d82fb3bbdd3ffde64
WoH <wolfgang.hobmaier@datapweb.de>
2016-10-18 18:03:59 +0200

    [RC1] ConfigService: Platform

--- ionic2/src/services/config.service.ts
+++ ionic2/src/services/config.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
 
 import env from '../app/env';
 
 @Injectable()
 export class ConfigService {
 
+  constructor(private platform: Platform) { }
+
   get apiEndpoint(): string {
-    if (env.BUILD_TARGET !== 'web' && env.API_ENDPOINT) {
-      return env.API_ENDPOINT;
-    } else {
-      // For build target 'web' we are running a node server alongside
-      // the src which proxies all api requests.
+    if (this.browserPlatform) {
+      // If we are running inside a browser the app is served
+      // by a web server which also proxies all API requests.
       return window.location.origin;
+    } else {
+      return env.API_ENDPOINT;
     }
   }
 
@@ -23,6 +26,10 @@ export class ConfigService {
       // the src which proxies all websocket requests.
       return window.location.origin;
     }
+  }
+
+  get browserPlatform(): boolean {
+    return this.platform.is('core') || this.platform.is('mobileweb');
   }
 
 }
