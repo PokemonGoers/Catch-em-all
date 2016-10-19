50771dbbc172cc5c0ba0120d71871e531675abac
WoH <wolfgang.hobmaier@datapweb.de>
2016-10-19 03:05:39 +0200

    [RC1] AppComponent: DI and template

--- ionic2/src/app/app.component.ts
+++ ionic2/src/app/app.component.ts
@@ -6,12 +6,12 @@ import 'rxjs/add/operator/map';
 import { MapPage } from '../pages/map/map.page';
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: './app.html'
 })
 export class App {
   rootPage = MapPage;
 
-  constructor(platform: Platform) {
+  constructor(private platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
