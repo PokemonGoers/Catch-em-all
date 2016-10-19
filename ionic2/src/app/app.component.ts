0a0c12cded6d331e67f90adf4eceb734f3e18ce7
mhartington <mikehartington@gmail.com>
2016-10-19 10:55:23 -0400

    feat(): manually hide splashscreen

--- ionic2/src/app/app.component.ts
+++ ionic2/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { StatusBar, Splashscreen } from 'ionic-native';
 import 'rxjs/add/operator/map';
 
 import { MapPage } from '../pages/map/map.page';
@@ -16,6 +16,7 @@ export class App {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      Splashscreen.hide();
     });
   }
 }
