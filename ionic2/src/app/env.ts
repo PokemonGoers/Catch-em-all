36961f5ed89f5b188a15849f7feedd4a6bb31b9b
WoH <wolfgang.hobmaier@datapweb.de>
2016-10-19 22:51:01 +0200

    [RC1] Force release env

--- ionic2/src/app/env.ts
+++ ionic2/src/app/env.ts
@@ -1,6 +1,6 @@
 // Exports global constants from build
 export default {
-  BUILD_ENV: 'develop',
+  BUILD_ENV: 'release',
   BUILD_TIME: new Date(),
   API_ENDPOINT: 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014',
   WEBSOCKET_ENDPOINT: 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65024'
