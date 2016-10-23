8b5e9e7807dae438e266453491f62cac35c7c1cb
WoH <wolfgang.hobmaier@datapweb.de>
2016-10-23 13:26:22 +0200

    [RC1] Reenable Proxying

--- ionic2/src/services/config.service.ts
+++ ionic2/src/services/config.service.ts
@@ -9,13 +9,29 @@ export class ConfigService {
   constructor(private platform: Platform) { }
 
   get apiEndpoint(): string {
+    if (this.browserPlatform) {
+      // If we are running inside a browser the app is served
+      // by a web server which also proxies all API requests.
+      return window.location.origin;
+    } else {
       return env.API_ENDPOINT;
+    }
   }
 
   get websocketEndpoint(): string {
+    if (this.browserPlatform) {
+      // If we are running inside a browser the app is served
+      // by a web server which also proxies all API requests.
+      return window.location.origin;
+    } else {
       // For build target 'web' we are running a node server alongside
       // the src which proxies all websocket requests.
       return env.WEBSOCKET_ENDPOINT;
+    }
+  }
+
+  get browserPlatform(): boolean {
+    return this.platform.is('core') || this.platform.is('mobileweb');
   }
 
   get isDevelopEnvironment(): boolean {
